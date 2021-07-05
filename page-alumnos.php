86ad8912e7ecfced554be95b55d8abd3e1aded31
jvsacancela <javiersacancela@gmail.com>
2021-07-05 17:42:33 -0500

    codigo de alumnos

---
+++ page-alumnos.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alumnos</title>
+    <link rel="stylesheet" href="style/general.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+</head>
+<body>
+    <div id="contenedor">
+       <h1>Alumnos</h1>
+        <div id="menu">
+           
+            <div id="contBuscar">
+                <i id="iconSearch" class="fas fa-search"></i>
+                <input type="text" id="inputBuscar" placeholder="Buscar">
+            </div>
+            <div id="acciones">
+                <a href="" id="btnAdd">Nuevo <i class="fas fa-user-plus"></i></a>
+            </div>
+        </div>
+           
+        
+        <table>
+            <thead>
+                <th></th>
+                <th>Foto</th>
+                <th>Código</th>
+                <th>Matricula</th>
+                <th>Folio</th>
+                <th>Cédula</th>
+                <th>Número</th>
+                <th>Apellidos</th>
+                <th>Nombre</th>
+                <th>Ret.</th>
+                <th>Paga %</th>
+                <th>Representante</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <a href=""><i class="fas fa-info" id="btnInfo"></i></a>
+                        <a href=""><i class="fas fa-edit" id="btnEdit"></i></a>
+                        <a href=""><i class="fas fa-trash" id="btnDelete"></i></a>
+                    </td>
+                    <td>abc</td>
+                    <td>abc</td>
+                    <td>abc</td>
+                    <td>abc</td>
+                    <td>abc</td>
+                    <td>abc</td>
+                    <td>abc</td>
+                    <td>abc</td>
+                    <td>abc</td>
+                    <td>abc</td>
+                    <td>abc</td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <a href=""><i class="fas fa-info" id="btnInfo"></i></a>
+                        <a href=""><i class="fas fa-edit" id="btnEdit"></i></a>
+                        <a href=""><i class="fas fa-trash" id="btnDelete"></i></a>
+                    </td>
+                    <td>abc</td>
+                    <td>abc</td>
+                    <td>abc</td>
+                    <td>abc</td>
+                    <td>abc</td>
+                    <td>abc</td>
+                    <td>abc</td>
+                    <td>abc</td>
+                    <td>abc</td>
+                    <td>abc</td>
+                    <td>abc</td>
+                   
+                </tr>
+            </tbody>
+
+            
+        </table>
+    </div>
+</body>
+</html>
---
+++ page-datos-alumnos.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Datos del alumno</title>
+    <link rel="stylesheet" href="style/general.css">
+    <link rel="stylesheet" href="style/datos.css">
+</head>
+<body>
+    <div id="contenedor">
+        <div id="tarjeta1">
+            <h1>Datos del alumno</h1>
+            <a href="">Editar datos</a>
+        </div>
+
+        <div id="tarjeta">
+            <h1>Identificacion</h1>
+
+            <img src="" alt="foto de perfil">
+            <p>Javier Vicente Sacancela Macas<p>
+        </div>
+
+        <div id="tarjeta">
+            <h1>Datos adicionales</h1>
+        </div>
+
+        <div id="tarjeta">
+            <h1>Datos personales</h1>
+        </div>
+
+        <div id="tarjeta">
+            <h1>Datos familiares</h1>
+        </div>
+
+        <div id="tarjeta">
+            <h1>Cliente</h1>
+        </div>
+
+        <div id="tarjeta">
+            <h1>Datos del representante</h1>
+        </div>
+
+        <div id="tarjeta">
+            <h1>Plantel donde procede</h1>
+        </div>
+
+        <div id="tarjeta">
+            <h1>Condicionales</h1>
+            <h1>Observación</h1>
+        </div>
+    </div>
+</body>
+</html>
