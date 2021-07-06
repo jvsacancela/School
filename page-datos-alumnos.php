fa0c125f8745e492e32edc3729546a8ea20f7fc4
jvsacancela <javiersacancela@gmail.com>
2021-07-06 00:23:03 -0500

    cambio finalin

--- page-datos-alumnos.php
+++ page-datos-alumnos.php
@@ -7,49 +7,162 @@
     <title>Datos del alumno</title>
     <link rel="stylesheet" href="css/general.css">
     <link rel="stylesheet" href="css/datos.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
 </head>
 <body>
-    <div id="contenedor">
-        <div id="tarjeta1">
-            <h1>Datos del alumno</h1>
-            <a href="">Editar datos</a>
+    <div id="contenido">
+
+        <div id="bloque">
+            <div id="tarjeta">
+
+            <div id="subtarjeta">
+                <h1>Datos del alumno</h1>
+                <a href=""  id="btnEditar"><i class="fas fa-edit"></i> Editar</a>
+            </div>    
+                <p>deadede</p>
+                <h1>dedewdew</h1>
+                <p>deadewwefrwf</p>
+              
+            </div>
+
+            <div id="tarjeta">
+            <div id="subtarjeta">
+                <h1>Identificacion</h1>
+                <a href=""  id="btnEditar"><i class="fas fa-edit"></i> Editar</a>
+            </div>
+                <img src="" alt="foto de perfil">
+                <p>Javier Vicente Sacancela Macas<p>
+            </div>
+        </div>
+        
+        <div id="bloque">
+            <div id="tarjeta">
+            <div id="subtarjeta">
+                <h1>Datos adicionales</h1>
+                <a href=""  id="btnEditar"><i class="fas fa-edit"></i> Editar</a>
+            </div>
+            </div>
+
+            <div id="tarjeta">
+                <div id="subtarjeta">
+                    <h1>Datos personales</h1>
+                    <a href=""  id="btnEditar"><i class="fas fa-edit"></i> Editar</a>
+                </div>
+
+                <div>
+                    <label for="">Apellidos: </label>
+                    <input type="text" value="mi apellido">
+                </div>
+
+                <div>
+                    <label for="">Nombre: </label>
+                    <input type="text" value="mi nombre">
+                </div>
+                
+                <div>
+                    <label for="">Cedula: </label>
+                    <input type="text">
+                </div>
+
+                <div>
+                    <label for="">Lugar de naciemiento: </label>
+                    <input type="text">
+                </div>
+
+                <div>
+                    <label for="">Nacionalidad: </label>
+                    <input type="text">
+                </div>
+
+                <div>
+                    <label for="">Domicilio: </label>
+                    <input type="text">
+                </div>
+
+                <div>
+                    <label for="">Telefono: </label>
+                    <input type="text">
+                </div>
+
+                <div>
+                    <label for="">Estado civil: </label><br>
+
+                    <input type="radio" name="" id="" value="Soltero">
+                    <label for="Soltero">Soltero</label>
+
+                    <input type="radio" name="" id="" value="Casado">
+                    <label for="Casado">Casado</label><br>
+
+                    <input type="radio" name="" id="" value="Divorciado">
+                    <label for="Divorciado">Divorciado</label>
+
+                    <input type="radio" name="" id="" value="Union-libre">
+                    <label for="Union-libre">Union libre</label>
+
+                    <input type="radio" name="" id="" value="Viudo">
+                    <label for="Viudo">Viudo</label><br>
+                </div>
+                
+                <div>
+                    <label for="">Sexo: </label><br>
+
+                    <input type="radio" name="" id="" value="Masculino">
+                    <label for="Masculino">Masculino</label>
+
+                    <input type="radio" name="" id="" value="Femenino">
+                    <label for="Femenino">Femenino</label>
+                </div>
+
+                <div>
+                    <label for="">Nuevo</label>
+                    
+                    <input type="checkbox" name="" id="" value="si">
+                </div>
+
+            </div>
+        </div>
+       
+        <div id="bloque">
+            <div id="tarjeta">
+            <div id="subtarjeta">
+                <h1>Datos familiares</h1>
+                <a href=""  id="btnEditar"><i class="fas fa-edit"></i> Editar</a>
+            </div>
+            </div>
+
+            <div id="tarjeta">
+                <div id="subtarjeta">
+                <h1>Cliente</h1>
+                <a href=""  id="btnEditar"><i class="fas fa-edit"></i> Editar</a>
+            </div>
+            </div>
         </div>
 
-        <div id="tarjeta">
-            <h1>Identificacion</h1>
+        
+        <div id="bloque">
+            <div id="tarjeta">
+            <div id="subtarjeta">
+                <h1>Datos del representante</h1>
+                <a href=""  id="btnEditar"><i class="fas fa-edit"></i> Editar</a>
+            </div>
+            </div>
 
-            <img src="" alt="foto de perfil">
-            <p>Javier Vicente Sacancela Macas<p>
+            <div id="tarjeta">
+            <div id="subtarjeta">
+                <h1>Plantel donde procede</h1>
+                <a href=""  id="btnEditar"><i class="fas fa-edit"></i> Editar</a>
+            </div>
+            </div>
+        </div>
+       
+        <div id="bloque">
+            <div id="tarjeta">
+                <h1>Condicionales</h1>
+                <h1>Observación</h1>
+            </div>
         </div>
 
-        <div id="tarjeta">
-            <h1>Datos adicionales</h1>
-        </div>
-
-        <div id="tarjeta">
-            <h1>Datos personales</h1>
-        </div>
-
-        <div id="tarjeta">
-            <h1>Datos familiares</h1>
-        </div>
-
-        <div id="tarjeta">
-            <h1>Cliente</h1>
-        </div>
-
-        <div id="tarjeta">
-            <h1>Datos del representante</h1>
-        </div>
-
-        <div id="tarjeta">
-            <h1>Plantel donde procede</h1>
-        </div>
-
-        <div id="tarjeta">
-            <h1>Condicionales</h1>
-            <h1>Observación</h1>
-        </div>
+        
     </div>
 </body>
 </html>
